5fa1f749c7ac249cef536c131d0086013b95bd6d
Cristian Cuartas <kristian_9411@hotmail.com>
2022-09-29 00:04:52 -0500

    punto 1 - add method textShort return cadena de 20 caracteres

--- prueba-sourcecol/Program.cs
+++ prueba-sourcecol/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Linq;
 
 namespace prueba_sourcecol
 {
     internal class Program
     {
+
+        static string textShort(string textFull)
+        {
+            string textShort = "";
+
+            if ( textFull.Length > 20 )
+            {
+                string[] textList = textFull.Split(' ');
+
+                int countCharacters = 0;
+
+                do
+                {
+                    foreach( string data in textList)
+                    {
+                        countCharacters += data.Length;
+
+                        if ( countCharacters < 20 )
+                        {
+                            textShort += data +" ";
+                        } else
+                        {
+                            if ( (countCharacters - 20 ) == 0)
+                            {
+                                textShort += data + " ";
+                            } else
+                            {
+                                textShort += data.Substring(0, (data.Length - (countCharacters  - 20)));
+                                textShort += "...";
+                            }
+                            break;
+                        }
+                        countCharacters++;
+                    }
+
+                } while (countCharacters < 20);
+
+            } else
+            {
+                return textFull;
+            }
+
+            return textShort;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string textFullPunt1 = "";
+
+            Console.WriteLine("punto 1");
+            textFullPunt1 = Console.ReadLine();
+
+            Console.WriteLine( textShort(textFullPunt1) );
+
         }
     }
 }
