411eaf3b995d3c5d5a76b89ea94f9563383104f3
Cristian Cuartas <kristian_9411@hotmail.com>
2022-09-29 00:14:58 -0500

    punto 2 - add isFibonacci return string cumple o no cumple

--- prueba-sourcecol/Program.cs
+++ prueba-sourcecol/Program.cs
@@ -4,9 +4,44 @@ namespace prueba_sourcecol
 {
     internal class Program
     {
+
+        static void isFibonacci(int number)
+        {
+            int number1 = 0;
+            int number2 = 1;
+            bool cumple = false;
+
+            for (int i = 0; i < number; i++)
+            {
+                int temp = number1;
+
+                number1 = number2;
+
+                number2 = temp + number1;
+
+                if (number1 == number)
+                {
+                    cumple = true;
+                }
+            }
+
+            if (cumple)
+            {
+                Console.WriteLine("El numero {0} cumple con Fibonacci ", number);
+            }
+            else
+            {
+                Console.WriteLine("El numero {0} no cumple con Fibonacci ", number);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberFibonacci = 0;
+
+            Console.WriteLine("punto 2");
+            numberFibonacci = Int32.Parse(Console.ReadLine());
+            isFibonacci(numberFibonacci);
         }
     }
 }
