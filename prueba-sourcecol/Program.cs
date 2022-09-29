c00372fd96f1b073faadbe0e6ee0c24343af72ed
Cristian Cuartas <kristian_9411@hotmail.com>
2022-09-28 22:49:20 -0500

    Add project files.

---
+++ prueba-sourcecol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prueba_sourcecol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
