f2937b93c8296db0c6cccf3991ecba0654e8d0af
Cristian Cuartas <kristian_9411@hotmail.com>
2022-09-29 21:26:31 -0500

    punto 5 - add getCharacters from rick and morty

--- prueba-sourcecol/Program.cs
+++ prueba-sourcecol/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace prueba_sourcecol
 {
     internal class Program
     {
+        static void getCharacters()
+        {
+            WebRequest request = WebRequest.Create("https://rickandmortyapi.com/api/character");
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+
+                dynamic json = JsonConvert.DeserializeObject(responseFromServer);
+
+                foreach (var e in json.results)
+                {
+                    Console.WriteLine(e.name);
+                }
+
+                // limpia
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+            catch (WebException err)
+            {
+                Console.WriteLine("Fallo {0}", err);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            getCharacters();
         }
     }
 }
