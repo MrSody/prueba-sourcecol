87034ff1834b4f79d129fa1c013ed7af62e753cb
Cristian Cuartas <kristian_9411@hotmail.com>
2022-09-29 20:45:19 -0500

    fix error

--- prueba-sourcecol/Program.cs
+++ prueba-sourcecol/Program.cs
@@ -5,62 +5,62 @@ using System.Linq;
 
 namespace prueba_sourcecol
 {
-    static List<Car> carModelMayorCinco(List<Car> listCar)
-    {
-        int modelMax = 2022;
-
-        var listCarMayorCinco = listCar.Where(x => x.Model >= (modelMax - 5)).ToList();
-
-        foreach (var dato in listCarMayorCinco)
-        {
-            Console.WriteLine("marca {0}", dato.Brand);
-            Console.WriteLine("modelo {0}", dato.Model);
-            Console.WriteLine("color {0}", dato.Color);
-        }
-
-        return listCar.Where(x => x.Model < (modelMax - 5)).ToList();
-    }
-
-    static List<Car> addCars()
-    {
-        bool addCar = true;
-        List<Car> listCar = new List<Car>() { };
-
-        do
-        {
-            Console.WriteLine("ingrese la marca");
-            string brand = Console.ReadLine();
-
-            Console.WriteLine("ingrese el modelo");
-            int model = Int32.Parse(Console.ReadLine());
-
-            Console.WriteLine("Ingrese el color");
-            string color = Console.ReadLine();
-
-            listCar.Add(
-                new Car()
-                {
-                    Brand = brand,
-                    Model = model,
-                    Color = color,
-                }
-            );
-
-            Console.WriteLine("Desea ingresar otro carro Y/N");
-            string newCar = Console.ReadLine();
-
-            if (newCar.ToLower() == "n")
-            {
-                addCar = false;
-            }
-
-        } while (addCar);
-
-        return listCar;
-    }
-
     internal class Program
     {
+        static List<Car> carModelMayorCinco(List<Car> listCar)
+        {
+            int modelMax = 2022;
+
+            var listCarMayorCinco = listCar.Where(x => x.Model >= (modelMax - 5)).ToList();
+
+            foreach (var dato in listCarMayorCinco)
+            {
+                Console.WriteLine("marca {0}", dato.Brand);
+                Console.WriteLine("modelo {0}", dato.Model);
+                Console.WriteLine("color {0}", dato.Color);
+            }
+
+            return listCar.Where(x => x.Model < (modelMax - 5)).ToList();
+        }
+
+        static List<Car> addCars()
+        {
+            bool addCar = true;
+            List<Car> listCar = new List<Car>() { };
+
+            do
+            {
+                Console.WriteLine("ingrese la marca");
+                string brand = Console.ReadLine();
+
+                Console.WriteLine("ingrese el modelo");
+                int model = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el color");
+                string color = Console.ReadLine();
+
+                listCar.Add(
+                    new Car()
+                    {
+                        Brand = brand,
+                        Model = model,
+                        Color = color,
+                    }
+                );
+
+                Console.WriteLine("Desea ingresar otro carro Y/N");
+                string newCar = Console.ReadLine();
+
+                if (newCar.ToLower() == "n")
+                {
+                    addCar = false;
+                }
+
+            } while (addCar);
+
+            return listCar;
+        }
+
         static void Main(string[] args)
         {
             List<Car> listCar = new List<Car>() { };
